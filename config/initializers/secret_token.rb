42dd58c9d6569a1b2dd0919e21204cf30907a116
tomasherrera <tomas.herrera@koombea.com>
2013-04-19 16:55:54 -0500

    First Commit ever made
    Signed-off-by: tomasherrera <tomas.herrera@koombea.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openshop::Application.config.secret_token = '9439bdab81127f5186ff6fe1ad6cd8a00c68840d4b92f3b80ba60f1fe7337304e1b356ca6fd47b1edc1a181280915df972c9cd7831c548e0d910f25a8243fc49'
